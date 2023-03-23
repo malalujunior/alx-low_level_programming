31c8ad55452d71d7fbc08aaf2e6ae04c546e9695
malalujunior <malaluedgar405@gmail.com>
2023-03-23 05:24:46 +0300

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+        int a, b, c, d;
+        for (a = 0; a <= 2; a++)
+        {
+                for (b = 0; b <= 9; b++)
+                {
+                        for (c = 0; c <= 5; c++)
+                        {
+                                for (d = 0; d <= 9; d++)
+                                {
+                                        if ((a <= 1 && b <= 9) || (a == 2 && b == 0))
+                                        {
+                                                _putchar(a + '0');
+                                                _putchar(b + '0');
+                                                _putchar(':');
+                                                _putchar(c + '0');
+                                                _putchar(d + '0');
+                                                _putchar('\n');
+                                        }
+                                }
+                        }
+                }
+        }
+}
