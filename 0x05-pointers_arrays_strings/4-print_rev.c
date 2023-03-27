1b8305e96ebfc407d991330476853cc4c54f1980
malalujunior <malaluedgar405@gmail.com>
2023-03-27 14:37:38 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,16 @@ void print_rev(char *s)
 {
 	int longi = 0;
 
-	int 0;
+	int o;
 
 	while (*s != '\0')
 	{
-		lomgi++;
+		longi++;
 		s++;
 	}
 	s--;
 
-	for (o = longi; 0 > 0; o--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
