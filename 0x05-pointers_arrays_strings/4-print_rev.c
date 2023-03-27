1624ac0555a5f0c9f41efc81cd34a4ebc6a74389
malalujunior <malaluedgar405@gmail.com>
2023-03-27 14:31:54 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+
+	int 0;
+
+	while (*s != '\0')
+	{
+		lomgi++;
+		s++;
+	}
+	s--;
+
+	for (o = longi; 0 > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
