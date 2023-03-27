b29c11e9750aca19e13f944a1006d5d9fc9baf9d
malalujunior <malaluedgar405@gmail.com>
2023-03-27 16:09:16 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -34,13 +34,15 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1} < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
+		}
+		i++;
 	}
-	i++;
-}
-if (f == 0)
-	return (0);
+	if (f == 0)
+		return (0);
+
 	return (n);
-	}
+}
+
